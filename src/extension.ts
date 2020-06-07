8c98796c814eeec64fd2cd5c3d84dd455b99c8e1
merlinye <merlin.ye@qq.com>
2020-06-07 20:32:27 +0800

    fix: remove redundant codes

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,8 @@ import * as vscode from 'vscode';
 import { pickProcess } from './pickProcess';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.nodeDebugProcessPicker.pick', async (args: any) => {
-		const result = await pickProcess();
-		console.log('pick result:', result);
-		vscode.window.showInformationMessage('Result:\n' + result);
-		return result;
+	let disposable = vscode.commands.registerCommand('extension.nodeDebugProcessPicker.pick', async () => {
+		return pickProcess();
 	});
 
 	context.subscriptions.push(disposable);
